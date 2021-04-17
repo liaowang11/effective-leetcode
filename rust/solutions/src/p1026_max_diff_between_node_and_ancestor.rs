0dcdb9f38a9372841d35256c82aa55776212558e
wliao <lwlw1988@gmail.com>
2021-04-17 21:39:37 +0800

    fix p1026 not updating new min and max

--- rust/solutions/src/p1026_max_diff_between_node_and_ancestor.rs
+++ rust/solutions/src/p1026_max_diff_between_node_and_ancestor.rs
@@ -23,29 +23,29 @@ impl TreeNode {
 struct Solution {}
 impl Solution {
     pub fn max_ancestor_diff(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let max = Rc::new(RefCell::new(i32::MIN));
+        let max_diff = Rc::new(RefCell::new(i32::MIN));
         if let Some(root) = root {
-            Self::max_ancestor_diff_rec(root, max.clone());
+            Self::max_ancestor_diff_rec(root, max_diff.clone());
         } else {
             return 0;
         }
-        return *max.borrow();
+        return *max_diff.borrow();
     }
-    fn max_ancestor_diff_rec(root: Rc<RefCell<TreeNode>>, max: Rc<RefCell<i32>>) -> (i32, i32) {
+    fn max_ancestor_diff_rec(root: Rc<RefCell<TreeNode>>, max_diff: Rc<RefCell<i32>>) -> (i32, i32) {
         let root_val = root.borrow().val;
         let left = root
             .borrow()
             .left
             .as_ref()
-            .map(|left| Self::max_ancestor_diff_rec(left.clone(), max.clone()));
+            .map(|left| Self::max_ancestor_diff_rec(left.clone(), max_diff.clone()));
         let right = root
             .borrow()
             .right
             .as_ref()
-            .map(|right| Self::max_ancestor_diff_rec(right.clone(), max.clone()));
+            .map(|right| Self::max_ancestor_diff_rec(right.clone(), max_diff.clone()));
         match (left, right) {
             (Some((left_min, left_max)), Some((right_min, right_max))) => {
-                let new_max = *[
+                let new_max_diff = *[
                     (left_max - root_val).abs(),
                     (left_min - root_val).abs(),
                     (right_min - root_val).abs(),
@@ -54,38 +54,41 @@ impl Solution {
                 .iter()
                 .max()
                 .unwrap_or(&i32::MIN);
-                if new_max > *(max.borrow()) {
-                    *max.borrow_mut() = new_max;
-                    println!(
-                        "Updating max to {:?} left_min={:?} left_max={:?} right_min={:?} right_max={:?} root_val={:?}",
-                        new_max, left_min, left_max, right_min, right_max, root_val
-                    );
+                println!(
+                    "left_min={:?} left_max={:?} right_min={:?} right_max={:?} root_val={:?}",
+                    left_min, left_max, right_min, right_max, root_val
+                );
+                if new_max_diff > *(max_diff.borrow()) {
+                    *max_diff.borrow_mut() = new_max_diff;
                 }
-                (left_min, right_max)
+                (std::cmp::min(std::cmp::min(root_val, left_min), right_min),
+                 std::cmp::max(std::cmp::max(root_val, left_max), right_max))
             }
             (None, Some((right_min, right_max))) => {
-                let new_max =
+                let new_max_diff =
                     std::cmp::max((right_max - root_val).abs(), (right_min - root_val).abs());
-                if new_max > *(max.borrow()) {
-                    *max.borrow_mut() = new_max;
-                    println!(
-                        "Updating max to {:?} right_min={:?} right_max={:?} root_val={:?}",
-                        new_max, right_min, right_max, root_val
-                    );
+                println!(
+                    "right_min={:?} right_max={:?} root_val={:?}",
+                    right_min, right_max, root_val
+                );
+                if new_max_diff > *(max_diff.borrow()) {
+                    *max_diff.borrow_mut() = new_max_diff;
                 }
-                (root_val, right_max)
+                (std::cmp::min(root_val, right_min),
+                 std::cmp::max(root_val, right_max))
             }
             (Some((left_min, left_max)), None) => {
-                let new_max =
+                let new_max_diff =
                     std::cmp::max((root_val - left_min).abs(), (root_val - left_max).abs());
-                if new_max > *(max.borrow()) {
-                    *max.borrow_mut() = new_max;
-                    println!(
-                        "Updating max to {:?} left_min={:?} left_max={:?} root_val={:?}",
-                        new_max, left_min, left_max, root_val
-                    );
+                println!(
+                    "left_min={:?} left_max={:?} root_val={:?}",
+                    left_min, left_max, root_val
+                );
+                if new_max_diff > *(max_diff.borrow()) {
+                    *max_diff.borrow_mut() = new_max_diff;
                 }
-                (left_min, root_val)
+                (std::cmp::min(root_val, left_min),
+                 std::cmp::max(root_val, left_max))
             }
             (None, None) => (root_val, root_val),
         }
@@ -135,6 +138,34 @@ mod tests {
         assert_eq!(
             Solution::max_ancestor_diff(Some(Rc::new(RefCell::new(node1)))),
             3
+        );
+    }
+
+    #[test]
+    fn test_max_diff_between_nodes_ancestor3() {
+        //[8,null,1,5,6,2,4,0,null,7,3]
+        let node7 = TreeNode::new(7);
+        let node3 = TreeNode::new(3);
+        let node4 = TreeNode::new(4);
+        let node0 = TreeNode::new(0);
+        let mut node6 = TreeNode::new(6);
+        node6.left = Some(Rc::new(RefCell::new(node0)));
+        let mut node2 = TreeNode::new(2);
+        node2.left = Some(Rc::new(RefCell::new(node7)));
+        node2.right = Some(Rc::new(RefCell::new(node3)));
+
+        let mut node5 = TreeNode::new(5);
+        node5.left = Some(Rc::new(RefCell::new(node2)));
+        node5.right = Some(Rc::new(RefCell::new(node4)));
+
+        let mut node1 = TreeNode::new(1);
+        node1.left = Some(Rc::new(RefCell::new(node5)));
+        node1.right = Some(Rc::new(RefCell::new(node6)));
+        let mut root = TreeNode::new(8);
+        root.right = Some(Rc::new(RefCell::new(node1)));
+        assert_eq!(
+            Solution::max_ancestor_diff(Some(Rc::new(RefCell::new(root)))),
+            8
         );
     }
 }
